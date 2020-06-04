e9549268a943c92a39029fe6f827bf03ca02751f
swethina <35322929+swethina@users.noreply.github.com>
2020-06-04 22:16:41 +0100

    Update Trello.py

--- Trello.py
+++ Trello.py
@@ -1,7 +1,7 @@
 import requests
 
 def getboards():
-    url = "https://api.trello.com/1/members/me/boards?key=83186eec77e3dfdef0c9564133966dc2&token=d4a9295330e07c0fb4fb595bf3df125705ddebcafde74aaf24e03d78709ae13f"
+    url = "https://api.trello.com/1/members/me/boards?key={yourkey}&token={yourtoken}"
     response = requests.request("get", url)
     url1 = response.url
     print(url1)
@@ -9,22 +9,22 @@ def getboards():
 
 
 def getsingleboard():
-    url="https://api.trello.com/1/boards/5ed7862b20647c8a7b0834b1?key=83186eec77e3dfdef0c9564133966dc2&token=d4a9295330e07c0fb4fb595bf3df125705ddebcafde74aaf24e03d78709ae13f"
+    url="https://api.trello.com/1/boards/{boardid}?key={yourkey}&token={yourtoken}"
     response = requests.request("get", url)
     print(response.text)
 
 def getfield():
-    url ="https://api.trello.com/1/members/me/boards?fields=name,url&key=83186eec77e3dfdef0c9564133966dc2&token=d4a9295330e07c0fb4fb595bf3df125705ddebcafde74aaf24e03d78709ae13f"
+    url ="https://api.trello.com/1/members/me/boards?fields=name,url&key={yourkey}&token={yourtoken}"
     response = requests.request("get", url)
     print(response.text)
 
 def getlist():
-    url="https://api.trello.com/1/lists/5ed788d37423d77bfc76213b?key=83186eec77e3dfdef0c9564133966dc2&token=d4a9295330e07c0fb4fb595bf3df125705ddebcafde74aaf24e03d78709ae13f"
+    url="https://api.trello.com/1/lists/{listid}?key={yourkey}&token={yourtoken}"
     response = requests.request("get", url)
     print(response.text)
 
 def putboard():
-    url="https://api.trello.com/1/boards/5ed7862b20647c8a7b0834b1?key=83186eec77e3dfdef0c9564133966dc2&token=d4a9295330e07c0fb4fb595bf3df125705ddebcafde74aaf24e03d78709ae13f"
+    url="https://api.trello.com/1/boards/{boardid}?key={yourkey}&token={yourtoken}"
     name ={
         'name' : 'qapitol'
     }
@@ -32,7 +32,7 @@ def putboard():
     print(response.text)
 
 def postboard():
-    url="https://api.trello.com/1/boards?key=83186eec77e3dfdef0c9564133966dc2&token=d4a9295330e07c0fb4fb595bf3df125705ddebcafde74aaf24e03d78709ae13f"
+    url="https://api.trello.com/1/boards?key={yourkey}&token={yourtoken}"
     name = {
         'name': 'qapitol4'
     }
@@ -40,7 +40,7 @@ def postboard():
     print(response.text)
 
 def getcard():
-    url="https://api.trello.com/1/lists/5ed78df251c9841b3ddb1e0a/cards?key=83186eec77e3dfdef0c9564133966dc2&token=d4a9295330e07c0fb4fb595bf3df125705ddebcafde74aaf24e03d78709ae13f"
+    url="https://api.trello.com/1/lists/{listid}/cards?key={yourkey}&token={yourtoken}"
     response = requests.request("get", url)
     print(response.text)
 
